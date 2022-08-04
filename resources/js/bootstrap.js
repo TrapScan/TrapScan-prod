ad9b035fdb07606e830c90c63464d3ba4264946a
apoliten <aponkinos@gmail.com>
2022-08-04 12:32:19 +0300

    update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,6 +23,8 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
+console.log(import.meta.env.VITE_PUSHER_APP_KEY)
+
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
