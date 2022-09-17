83630d505ecfe9d941dcb3a8d66a36fe68d1a22b
apoliten <aponkinos@gmail.com>
2022-09-17 23:14:12 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+const path = require('path');
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
     plugins: [
+        viteCompression(),
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
@@ -17,7 +20,20 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve('resources/js'),
+            ziggy: path.resolve('vendor/tightenco/ziggy/src/js'),
+        },
+        css: {
+            devSourcemap: true,
+        },
+    },
     build: {
         chunkSizeWarningLimit: 1600,
+        sourcemap: true,
+        rollupOptions: {
+            external: 'ziggy'
+        }
     },
 });
