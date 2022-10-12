489a880b2d81193c7478a1ca1704e6a2f9e811b5
apoliten <aponkinos@gmail.com>
2022-10-13 00:55:34 +0300

    update

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -38,6 +38,7 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'admin'=> (\Auth::check() ? \Auth::user()->hasRole('admin') : false),
             'flash' => [
                 'message' => fn () => $request->session()->get('message')
             ],
