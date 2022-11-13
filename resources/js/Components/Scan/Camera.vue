b42943fba61209d632c6794d789f1c78c80279fc
apoliten <aponkinos@gmail.com>
2022-11-14 01:41:44 +0200

    fix menu

--- resources/js/Components/Scan/Camera.vue
+++ resources/js/Components/Scan/Camera.vue
@@ -43,7 +43,7 @@
                               <span class="z-10 h-full leading-snug font-normal absolute text-center text-slate-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
                               <svg width="28" height="28" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"></path></svg>
                               </span>
-                    <input v-model="form.qr_id" type="text" class="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-lg border-0 shadow outline-none focus:outline-none focus:ring w-full pl-10"/>
+                    <input v-on:keyup.enter="submit_form" v-model="form.qr_id" type="text" class="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-lg border-0 shadow outline-none focus:outline-none focus:ring w-full pl-10"/>
                 </div>
                 <div class="flex">
                     <div class="flex w-5/12 mx-auto">
@@ -115,7 +115,7 @@ export default {
                     this.error = "browser seems to be lacking features"
                 }
             } finally {
-                // hide loading indicator 
+                // hide loading indicator
                 this.loading = false
             }
         },
